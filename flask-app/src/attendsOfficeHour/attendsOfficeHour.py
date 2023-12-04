e2f8cd6d74d5d735a5f4263da22721302da15f87
wyattchris <chris.wyatt321@gmail.com>
2023-12-04 14:05:34 -0500

    attends_OH CRUD pt2

--- flask-app/src/attendsOfficeHour/attendsOfficeHour.py
+++ flask-app/src/attendsOfficeHour/attendsOfficeHour.py
@@ -6,7 +6,7 @@ attends_office_hour = Blueprint('attends_office_hour', __name__)
 
 # Get all attends_office_hour from StudyStage
 @attends_office_hour.route('/all', methods=['GET'])
-def get_attends_office_hours():
+def get_attends_attends_office_hour():
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM Attends_OH')
     row_headers = [x[0] for x in cursor.description]
@@ -24,3 +24,70 @@ def get_attends_office_hours():
     the_response.mimetype = 'application/json'
 
     return the_response
+
+# create an office hours 
+@attends_office_hour.route('/create-attendsOH', methods=['POST'])
+def add_attends_office_hour():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    StudentID = the_data['StudentID']
+    OfficeHoursID = the_data['OfficeHoursID']
+    AssignmentID = the_data['AssignmentID']
+
+    # Constructing the query
+    query = 'insert into Attends_OH (StudentID, OfficeHoursID, AssignmentID) VALUES ("'
+    query += str(StudentID) + '", "'
+    query += str(OfficeHoursID) + '", "'
+    query += str(AssignmentID) + ')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Successfully created a new Attends Office Hours!'
+
+# Update information about an office hours
+@attends_office_hour.route('/update-attendsOH/<attends_office_hourID>', methods=['PUT'])
+def update_attends_office_hour(attends_office_hourID):
+
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    StudentID = the_data['StudentID']
+    OfficeHoursID = the_data['OfficeHoursID']
+    AssignmentID = the_data['AssignmentID']
+
+    # constructing the query
+    query = 'UPDATE Attends_OH SET StudentID = "'
+    query += str(StudentID) + '", OfficeHoursID = "'
+    query += str(OfficeHoursID) + '", CourseCode = "'
+    query += str(AssignmentID) + ' WHERE StudentID = '
+    query += str(attends_office_hourID)
+
+    # executing and committing the update statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Office Hours information updated successfully!'
+
+# Delete an office hours
+@attends_office_hour.route('/delete-officehours/<attends_office_hourID>', methods=['DELETE'])
+def delete_attends_office_hour(attends_office_hourID):
+    # constructing the query
+    query = 'DELETE FROM Attends_OH WHERE StudentID = ' + str(attends_office_hourID)
+
+    # executing and committing the delete statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'OfficeHours deleted successfully!'
