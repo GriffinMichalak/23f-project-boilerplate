45bd61d4a6ba45d1ed5e889b0d70681d4c010010
GriffinMichalak <griffinmichalak@gmail.com>
2023-12-04 21:15:47 -0500

    create attends_group

--- flask-app/src/attendsGroup/attendsGroup.py
+++ flask-app/src/attendsGroup/attendsGroup.py
@@ -8,7 +8,7 @@ attends_group = Blueprint('attends_group', __name__)
 @attends_group.route('/all', methods=['GET'])
 def get_attends_group():
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM Attends_OH')
+    cursor.execute('SELECT * FROM Attends_Group')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -22,3 +22,45 @@ def get_attends_group():
 
     return the_response
 
+# gets a specific Attends_OH tuple 
+@attends_group.route('/get/<sID>', methods=['GET'])
+def get_specific_attends_group(sID):
+    query = 'SELECT StudentID, StudyGroupID FROM Attends_Group WHERE StudentID = '
+    query += str(sID)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    result = cursor.fetchone()
+
+    if result is None:
+        return 'No attends_group record found with the specified IDs.'
+
+    student_id = result[0]
+    group_id = result[1]
+
+    attends_oh_data = {
+        'StudentID': student_id,
+        'GroupID': group_id
+    }
+
+    return attends_oh_data
+
+@attends_group.route('/create', methods=['POST'])
+def create_attends_oh():
+    # Collecting data from the request object
+    the_data = request.json
+
+    # Extracting the variables
+    StudentID = the_data['StudentID']
+    StudyGroupID = the_data['GroupID']
+
+    # Constructing the query
+    query = 'INSERT INTO Attends_Group (StudentID, StudyGroupID) VALUES ('
+    query += str(StudentID) + ', ' + str(StudyGroupID)  + ')'
+
+    # Executing and committing the insert statement
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Attends_Group record created successfully!'
